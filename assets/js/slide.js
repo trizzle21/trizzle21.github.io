892a178561476b86a1e802edec0c268d37eaf016
trizzle21 <tdross21@gmail.com>
2017-06-30 08:58:14 -0400

    added layout, scss file and new slide.js

---
+++ assets/js/slide.js
@@ -0,0 +1,32 @@
+/*
+	Javascript module for About.
+
+
+
+
+*/
+
+;(function(){
+    var slide = {
+    	var slide_cont : 0,
+    	init : function(){
+			this.cacheDOM();
+			this.bindEvents();
+			this.render();
+
+    	},
+        cacheDOM: function(){
+        	this.$slide = $('.slide')
+        },
+        bindEvents: function(){
+
+        },
+        render: function(){
+
+        },
+
+
+
+    }
+  	slide.init()
+  });
