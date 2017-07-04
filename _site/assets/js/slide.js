a55e2a732201ff71cdcde9b2a243792c4d7a78da
trizzle21 <tdross21@gmail.com>
2017-07-04 11:32:55 -0700

    finished redesign, changed small sections of work page and updated paragraph

--- _site/assets/js/slide.js
+++ _site/assets/js/slide.js
@@ -25,20 +25,32 @@
             this.$arrow = $('.arrow');
             this.$slideContainer = $('.slide-container');
             this.$head = $('.head');
+            this.$arrowText = $('.arrow-text');
 
         },
         bindEvents: function(){
             
             this.$downArrow.on('click', this.$arrow, this.changeSlide.bind(this, 'down'));
 			//this.$bodyTag.on('scroll', this.$arrow, this.changeSlide.bind(this, 'down'));
-
+            // this.$bodyTag.on('mousewheel', this.detectScroll.bind(this));
             // this.$slideContainer.on('click', this.$upArrow, this.changeSlide(this, 'up'))
         },
         render: function(){
         },
+        detectScroll:function(event){
+            if (event.originalEvent.wheelDelta >= 0) {
+                return
+            }
+            else {
+                this.changeSlide('down');
+            }
+        },
         changeSlide: function(direction) {
             if (this.slide_count > this.max_count){
                 return;
+            }
+            if(this.slide_count == 0){
+                this.$arrowText.removeClass('visible').addClass('hidden');
             }
             count = '.slide-' + this.slide_count;
             current_slide = this.$slideContainer.find(count);
@@ -51,7 +63,7 @@
             } else{
                 fade = 2000
             }
-
+            
             if(direction == 'up'){
         		this.slide_count -= 1;
         	} else {
@@ -59,6 +71,10 @@
         	}
             next_slide = this.$slideContainer.find('.slide-' + this.slide_count);
             next_slide.hide().fadeIn(fade);
+
+            setTimeout(function(){
+                this.scroll = false;
+            }, 5000)
 
             current_slide.css('transform: translate3d(0px, 100%, 0px);');
 
