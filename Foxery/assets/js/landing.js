dc3ca8d46f3d8533018816ac7158df73d68f8ba8
trizzle21 <tdross21@gmail.com>
2017-10-24 16:08:51 -0400

    added Foxery as demo for Lance

---
+++ Foxery/assets/js/landing.js
@@ -0,0 +1,95 @@
+/*
+
+
+*/
+
+
+;(function(){
+    var launch = {
+        activeSlide: 0,
+        init: function(){
+            this.cacheDOM();
+            this.bindEvents();
+            this.render();
+        },
+        cacheDOM: function(){
+            this.$scrollButton = $(".scroll-button");
+            this.$slideSelector = $(".slide-selector");
+
+
+        },
+        bindEvents: function(){
+            this.$scrollButton.on('click', this.scrollToTop.bind(this));
+            this.$slideSelector.on('click', this.changeSlideEvent.bind(this));     
+
+        },
+        render: function(){
+            this.interval = setInterval(this.changeNextSlide.bind(this), 3000);
+
+        },
+        scrollToTop:function(){
+            $( document ).scrollTop(0);
+        },
+        changeSlideEvent:function(){
+            var clickedButton = $(event.target);
+            clearInterval(this.interval);
+            if(clickedButton.data().roleSlide == 0 || clickedButton.data().roleSlide){
+                this.changeSlide(clickedButton.data()['roleSlide']);
+                this.interval = setInterval(this.changeNextSlide.bind(this), 3000);
+
+
+            }else {
+                clickedButton = clickedButton.closest('div');
+                if(clickedButton.data().roleSlide == 0 || clickedButton.data().roleSlide){
+                    this.changeSlide(clickedButton.data()['roleSlide']);
+                    this.interval = setInterval(this.changeNextSlide.bind(this), 3000);
+
+                }
+            }
+
+        },
+        changeSlide:function(slide_count){
+            var currentSlide = $('#slide-' + this.activeSlide);
+            var nextSlide = $('#slide-' + slide_count);
+
+            this.activeSlide = slide_count;
+            currentSlide.removeClass('active');
+            nextSlide.addClass('active');
+            
+            $('.selected').removeClass('selected');
+            $('#slide-selector-' + slide_count).addClass('selected');
+
+        },
+        changeNextSlide:function(){
+            slide_count = this.activeSlide + 1;
+            var overCheck = false;
+            if(slide_count == 3){
+                overCheck = true
+                slide_count = 0;
+            }
+        
+            var currentSlide = $('#slide-' + this.activeSlide);
+            var nextSlide = $('#slide-' + slide_count);
+            $('.selected').removeClass('selected');
+            $('#slide-selector-' + slide_count).addClass('selected');
+
+            if (overCheck){
+                this.activeSlide = 0
+
+            }else {
+                this.activeSlide = this.activeSlide+1;
+            }
+
+            currentSlide.removeClass('active');
+            nextSlide.addClass('active');
+            
+
+        }
+
+
+    }    
+
+    launch.init();
+
+
+})();
---
+++ Foxery/assets/js/sidenav.js
@@ -0,0 +1,11 @@
+// Functions that open and close nav bar, navbar css is set between 100% and 0
+// 
+
+function openNav() {
+    document.getElementById("main-sidenav").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("main-sidenav").style.width = "0";
+}
