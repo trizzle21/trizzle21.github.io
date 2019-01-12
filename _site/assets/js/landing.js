0e3ea03152e16a2f362f8a1ece24e603afa42cfb
trizzle21 <tdross21@gmail.com>
2019-01-11 23:49:34 -0500

    website redesign part 1

--- _site/assets/js/landing.js
+++ _site/assets/js/landing.js
@@ -1,54 +1,11 @@
+// Functions that open and close nav bar, navbar css is set between 100% and 0
+// 
 
-(function(){
-    var launch = {
-        init: function(){
-			this.cacheDOM();
-			this.bindEvents();
-			this.render();
-		},
-        cacheDOM: function(){
-            this.$body = $("#full");
-            
-            this.$about = this.$body.find("#About");
-            this.$work = this.$body.find("#Work");
-            this.$blog = this.$body.find("#Blog");
-            this.$contact = this.$body.find("#Contact");
-            
-            this.$sub_about = this.$about.find("#who");
-            this.$sub_work = this.$work.find("#what");
-            this.$sub_blog = this.$blog.find("#talk");
-            this.$sub_contact = this.$contact.find("#reach");
-            
-        },
-        bindEvents : function(){
-            this.$about.on("mouseenter mouseleave", this.$about, this.show.bind(this, this.$sub_about));
-            this.$work.on("mouseenter mouseleave", this.$work, this.show.bind(this,this.$sub_work ));
-            this.$blog.on("mouseenter mouseleave", this.$blog, this.show.bind(this,this.$sub_blog ));
-            this.$contact.on("mouseenter mouseleave", this.$contact, this.show.bind(this,this.$sub_contact ));
-        },
-        render : function(){
-            //Might not have anything yet  
-            this.$sub_about.toggle();
-            this.$sub_work.toggle();
-            this.$sub_blog.toggle();
-            this.$sub_contact.toggle();
+function openNav() {
+    document.getElementById("main-sidenav").style.width = "100%";
+}
 
-        },
-        show : function(jqloc){
-            jqloc.toggle();
-            $.each([this.$sub_about, this.$sub_work, this.$sub_blog, this.$sub_contact], function( index, value ) {
-                if(jqloc == value){
-                    value.closest('div').css('color','black')
-                } else {
-                    value.closest('div').css('color','grey')
-                }
-            
-            });
-
-
-        }
-        
-        
-    }
-    launch.init();
-})()
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("main-sidenav").style.width = "0";
+}
